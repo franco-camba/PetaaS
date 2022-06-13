b5455aa0d04da0c08b9df3f9ea595c1407b1dfa6
nickyoung-hashicorp <nyoung@hashicorp.com>
2022-06-13 08:34:48 -0700

    changes

--- main.tf
+++ main.tf
@@ -1,12 +1,48 @@
-resource "random_pet" "console_password" {
-    length = 4
-    prefix = var.prefix
-}
-
 variable "prefix" {
     default = "default"
 }
 
+resource "random_pet" "first" {
+    length = 10
+    prefix = var.prefix
+}
+
 output "pet_name" {
-    value = random_pet.console_password.id
+    value = random_pet.first.id
+}
+
+resource "random_pet" "second" {
+    length = 10
+    prefix = var.prefix
+}
+
+output "pet_name" {
+    value = random_pet.second.id
+}
+
+resource "random_pet" "third" {
+    length = 10
+    prefix = var.prefix
+}
+
+output "pet_name" {
+    value = random_pet.third.id
+}
+
+resource "random_pet" "fourth" {
+    length = 10
+    prefix = var.prefix
+}
+
+output "pet_name" {
+    value = random_pet.fourth.id
+}
+
+resource "random_pet" "fifth" {
+    length = 10
+    prefix = var.prefix
+}
+
+output "pet_name" {
+    value = random_pet.fifth.id
 }
